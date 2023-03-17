4aad43f4dade9f0c3c2cf05c2d8f6e2bcb952641
Jean-Baptiste Landry <jb.landry@valtech.com>
2023-03-17 14:39:21 -0400

    Reduce configuration to minimum

--- test/utils/generate-fixtures.js
+++ test/utils/generate-fixtures.js
@@ -8,12 +8,14 @@ const sortJson = require("sort-json");
 const root = path.join(__dirname, "..", "..");
 const packageConfig = fsSync.readJson(path.join(root, "package.json"));
 
-Object.entries(packageConfig.exports).forEach(async ([name, file]) => {
-	const id = String(name.slice(2)).split("/").join("-");
+Object.entries(packageConfig.exports)
+	.filter(([name]) => name !== ".")
+	.forEach(async ([name, file]) => {
+		const id = String(name.slice(2)).split("/").join("-");
 
-	const rawConfig = require(path.join(root, ...file.split("/")));
+		const rawConfig = require(path.join(root, ...file.split("/")));
 
-	const config = sortJson(rawConfig, { depth: 100 });
+		const config = sortJson(rawConfig, { depth: 100 });
 
-	fsSync.writeJson(path.join(root, "test", "feature", "fixtures", "config", `${id}.json`), config, { space: 2 });
-});
+		fsSync.writeJson(path.join(root, "test", "feature", "fixtures", "config", `${id}.json`), config, { space: 2 });
+	});
